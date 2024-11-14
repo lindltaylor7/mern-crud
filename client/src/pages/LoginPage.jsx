7c7a7635d1b52362a8f2a89ca89b2be5aa2945b2
lindltaylor7 <riuyagami@gmail.com>
2024-11-14 17:30:44 -0500

    update

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,6 +1,56 @@
+import { useForm } from "react-hook-form"
+import { useAuth } from "../context/AuthContext";
+import { Link } from "react-router-dom";
 function LoginPage() {
+
+  const { register, handleSubmit, formState:{ errors } } = useForm()
+
+  const { signin, errors: signInErrors } = useAuth()
+
+  const onSubmit = handleSubmit(data =>{
+    signin(data);
+  })
+
   return (
-    <div>LoginPage</div>
+    <div className="flex h-[calc(100vh-100px)] items-center justify-center">
+      <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md my-2">
+      {
+        signInErrors.map((error, i) => (
+          <div className="bg-red-500 p-2 text-white text-center my-2" key={i}>
+            {error}
+          </div>
+        ))
+      }
+      <h1 className="text-4xl font-bold">Login</h1>
+
+      <form onSubmit={onSubmit}>
+          <input type="email" {...register("email", {required:true})} 
+            className="w-full bg-zinc-700 text-white ps-4 py-2 rounded-md my-2"
+            placeholder="Email"
+          />
+          { errors.email && (
+            <p className="text-red-500">Email is required</p>
+          ) }
+
+
+          <input type="password" {...register("password", {required:true})}
+            className="w-full bg-zinc-700 text-white ps-4 py-2 rounded-md my-2"
+            placeholder="Password"
+          />
+          { errors.password && (
+            <p className="text-red-500">Password is required</p>
+          ) }
+
+
+          <button type="submit">
+            Login
+          </button>
+      </form>
+      <p className="flex gap-x-2 justitfy-between">
+        Dont have an account? <Link to="/register" className="text-sky-500"> Sign up </Link>
+      </p>
+      </div>
+    </div>
   )
 }
 
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,6 +1,65 @@
+import { useForm } from "react-hook-form"
+import { useAuth } from "../context/AuthContext"
+import { useEffect } from "react"
+import { useNavigate, Link } from "react-router-dom"
+
 function RegisterPage() {
+  const {register, handleSubmit, formState: { errors }} = useForm()
+  const { signup, isAuthenticated, errors: registerErrors } = useAuth()
+  const navigate = useNavigate()
+
+  useEffect(()=>{
+    if(isAuthenticated) navigate('/tasks')
+  },[isAuthenticated])
+
+  const onSubmit = handleSubmit(async(values)=>{
+    signup(values)
+  })
+
   return (
-    <div>RegisterPage</div>
+    <div className="bg-zinc-800 max-w-md p-10 rounded-md">
+      {
+        registerErrors.map((error, i) => (
+          <div className="bg-red-500 p-2 text-white" key={i}>
+            {error}
+          </div>
+        ))
+      }
+      <form onSubmit={onSubmit}>
+        <input type="text" {...register("username", {required:true})} 
+          className="w-full bg-zinc-700 text-white ps-4 py-2 rounded-md my-2"
+          placeholder="Username"
+        />
+        { errors.username && (
+          <p className="text-red-500">Username is required</p>
+        ) }
+
+        <input type="email" {...register("email", {required:true})} 
+          className="w-full bg-zinc-700 text-white ps-4 py-2 rounded-md my-2"
+          placeholder="Email"
+        />
+        { errors.email && (
+          <p className="text-red-500">Email is required</p>
+        ) }
+
+
+        <input type="password" {...register("password", {required:true})}
+          className="w-full bg-zinc-700 text-white ps-4 py-2 rounded-md my-2"
+          placeholder="Password"
+        />
+        { errors.password && (
+          <p className="text-red-500">Password is required</p>
+        ) }
+
+
+        <button type="submit">
+          Register
+        </button>
+      </form>
+      <p className="flex gap-x-2 justitfy-between">
+       Already have an account? <Link to="/login" className="text-sky-500"> Log in </Link>
+      </p>
+    </div>
   )
 }
 
