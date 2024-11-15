926a3ea1dfb48de4c7d324738ca3135ea0398f21
lindltaylor7 <riuyagami@gmail.com>
2024-11-15 16:53:29 -0500

    deploy settings

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@ import app from "./app.js"
 import { connectDB } from "./db.js"
 
 connectDB()
+
+// Sirve los archivos estáticos del frontend
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// Ruta para manejar las demás solicitudes y enviar el frontend
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
+
 app.listen(3000)
 console.log('Server on port', 3000)
