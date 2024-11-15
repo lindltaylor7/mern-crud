602d3152bb18019650b080149615ade40ccf37ef
digitechgrouperu <digitechgrouperu@gmail.com>
2024-11-15 17:58:45 -0500

    test deploy url

--- client/src/pages/TasksPage.jsx
+++ client/src/pages/TasksPage.jsx
@@ -1,26 +1,25 @@
-import { useEffect } from "react"
-import { useTask } from "../context/TaskContext"
-import TaskCard from "../components/TaskCard"
+import { useEffect } from "react";
+import { useTask } from "../context/TaskContext";
+import TaskCard from "../components/TaskCard";
 
 function TasksPage() {
-
-  const { getTasks, tasks } = useTask()
+  const { getTasks, tasks } = useTask();
 
   useEffect(() => {
-    getTasks()
-  }, [])
+    getTasks();
+  }, []);
 
-  if(tasks.length === 0) return (<h1>No tasks</h1>)
+  console.log(tasks);
 
-  return (  
+  if (tasks.length === 0) return <h1>No tasks</h1>;
+
+  return (
     <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-2">
-      {
-        tasks.map(task => (
-          <TaskCard task={task} key={task._id}/>
-        ))
-      }
+      {tasks.map((task) => (
+        <TaskCard task={task} key={task._id} />
+      ))}
     </div>
-  )
+  );
 }
 
-export default TasksPage
+export default TasksPage;
