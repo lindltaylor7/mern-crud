54cde04b4cb86b568bfa998bf9bcb23194c4890d
lindltaylor7 <riuyagami@gmail.com>
2024-11-15 17:14:10 -0500

    fix url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://mern-crud-olcs.onrender.com:3000/api",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
